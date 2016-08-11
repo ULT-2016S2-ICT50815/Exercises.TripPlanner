96712d4eee0d8a1ff66d3ab89995da0f76f8275f
Faisal Badani Balooch <faisal.balooch@gmail.com>
2016-08-11 15:13:48 +1000

    Added dbcontext

---
+++ src/TripPlanner/DataAccess/Models/TripPlannerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TripPlanner.DataAccess.Models
+{
+    public class TripPlannerDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; 
+    }
+}
