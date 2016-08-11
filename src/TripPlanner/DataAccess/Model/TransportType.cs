8c51187212a40eaf9b2aba9265ed5b83a12aa4f1
Henry Dinh <henry.dinh@hotmail.com>
2016-08-11 12:52:58 +1000

    Added Entities

---
+++ src/TripPlanner/DataAccess/Model/TransportType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TripPlanner.DataAccess.Model
+{
+    public class TransportType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Trip> Trips { get; set; }
+    }
+}
