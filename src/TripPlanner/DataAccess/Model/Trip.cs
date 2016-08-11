fd355e4baed6b3a1bb6861dc9b803d5c8ef93780
p015 <p015@UMITVIEWS-25>
2016-08-11 16:22:19 +1000

    DataAccess

---
+++ src/TripPlanner/DataAccess/Model/Trip.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TripPlanner.DataAccess.Model
+{
+    public class Trip
+    {
+        public int Id { get; set; }
+        public DateTime TripDate { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public string TransportTypeId { get; set; }
+        public TransportType TransportType { get; set; }
+    }
+}
