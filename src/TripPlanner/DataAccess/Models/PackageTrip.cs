aabbb7d47ec29865d518670147888fd3a1fac32e
Faisal Badani Balooch <faisal.balooch@gmail.com>
2016-08-11 14:51:50 +1000

    Faisal

---
+++ src/TripPlanner/DataAccess/Models/PackageTrip.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TripPlanner.DataAccess.Models
+{
+    public class PackageTrip
+    {
+        public int Id { get; set; }
+        public int PackageId { get; set; }
+        public int TripId { get; set; }
+        public decimal TripCost { get; set; 
+    }
+}
