6e3e322e3c7942d77b3e40c0de58cd62236d2f6b
[Harold Baldwin] <[haroldbaldwin@hotmail.com]>
2016-08-11 13:08:54 +1000

    Fixed Navigational Properties

--- src/TripPlanner/DataAccess/Models/Customer.cs
+++ src/TripPlanner/DataAccess/Models/Customer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 
 namespace TripPlanner.DataAccess.Models
@@ -8,12 +9,18 @@ namespace TripPlanner.DataAccess.Models
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public int LastName { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }       
+        //What if a customer is a traveller?
         public string StreetAddress { get; set; }
         public string Suburb { get; set; }
-        public int State { get; set; }
-        public int Postcode { get; set; }
+        public string State { get; set; }
+        public string Postcode { get; set; }
+        //Navigational Properties!!
+        public List<Package> Packages { get; set; }
     }
 }
