8536624c04e1f8651bc61c9b85c5eeb988312d04
P000 <jasonko96@gmail.com>
2016-08-11 16:12:27 +1000

    Added TripPlannerDbContext class

---
+++ src/TripPlanner/DataAccess/Models/PackageTrip.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TripPlanner.DataAccess.Models
+{
+    public class PackageTrip
+    {
+        public int Id { get; set; }
+        public int PackageId { get; set; }
+        public int TripId { get; set; }
+        public double TripCost { get; set; }
+        public Package Packages { get; set; }
+    }
+}
