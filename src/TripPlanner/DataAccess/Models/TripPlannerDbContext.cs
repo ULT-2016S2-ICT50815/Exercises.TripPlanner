9ecb49c9b07110bbb6dc273334d44e6c8aed11e0
Faisal Badani Balooch <faisal.balooch@gmail.com>
2016-08-12 11:10:48 +1000

    Full dbset, minor fixes

--- src/TripPlanner/DataAccess/Models/TripPlannerDbContext.cs
+++ src/TripPlanner/DataAccess/Models/TripPlannerDbContext.cs
@@ -9,5 +9,9 @@ namespace TripPlanner.DataAccess.Models
     public class TripPlannerDbContext : DbContext
     {
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Package> Packages { get; set; }
+        public DbSet<PackageTrip> PackageTrips  { get; set; }
+        public DbSet<TransportType> TransportTypes { get; set; }
+        public DbSet<Trip> Trips { get; set; }
     }
 }
